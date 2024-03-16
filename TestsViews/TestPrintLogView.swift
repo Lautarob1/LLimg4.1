049c9e88852581f88a568003e754d9a2377973e4
LBarrera <lautarobarrerab@gmail.com>
2024-03-15 23:43:05 -0400

    as of 15Mar2024 10 pm

---
+++ TestsViews/TestPrintLogView.swift
@@ -0,0 +1,18 @@
+//
+//  TestPrintLogView.swift
+//  LLimg_4
+//
+//  Created by EFI-Admin on 3/13/24.
+//
+
+import SwiftUI
+
+struct TestPrintLogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestPrintLogView()
+}
